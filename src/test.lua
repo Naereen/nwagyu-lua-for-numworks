a0e14eeea270f910dc9a2840370f665095b7b127
Lilian Besson <lilian.besson@crans.org>
2025-05-20 19:13:56 +0200

    Include storage.{c,h} from https://framagit.org/Yaya.Cout/numworks-extapp-storage/

--- src/test.lua
+++ src/test.lua
@@ -1,6 +1,13 @@
-for j=1,20 do
+-- This is NOT a Python script
+-- This is a Lua file!
+
+print("Hello world from Lua!")
+
+for j=1,5 do
+
   for i=1,10 do
     eadk.display_draw_string("Hello", math.sqrt(10*i), 10*i)
   end
+
   eadk.timing_msleep(1000)
 end
